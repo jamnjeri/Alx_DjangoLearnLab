f1682a425fd72b2d0956ad05d54368ba32f812c1
Jamila <jamnjeri@gmail.com>
2024-08-18 22:30:18 +0300

    Task 3 first commit

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,7 +1,8 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -29,3 +30,26 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserProfile(models.Model):
+    USER_ROLES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=USER_ROLES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+    
