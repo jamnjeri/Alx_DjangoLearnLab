541134c1d0633e3045b41a99f2d72fa7fdf9fcc6
Jamila <jamnjeri@gmail.com>
2024-09-07 13:01:04 +0300

    Views Updated to use generics and not viewsets

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,14 +7,22 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 
 # This view lists all books or someone can create a new book
-# class BookListView():
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
 
 #  This view retrieves, updates or deletes a single book instance
-# class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
 
 # ModelViewSet in DRF is a powerful and flexible view that automatically provides a full set of CRUD operations by default based on the queryset and serializer you provide
 # List:           GET /books/
@@ -24,28 +32,28 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Destroy:        DELETE /books/<id>/
 
 # Updated to use viewsets instead:
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookViewSet(viewsets.ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
-    # Customizing the View Behavior
-    def create(self, request, *args, **kwargs):
-        # Custom logic before creating a book
-        response = super().create(request, *args, **kwargs)
-        # Custom logic after creating a book
-        return response
+#     # Customizing the View Behavior
+#     def create(self, request, *args, **kwargs):
+#         # Custom logic before creating a book
+#         response = super().create(request, *args, **kwargs)
+#         # Custom logic after creating a book
+#         return response
     
-    def update(self, request, *args, **kwargs):
-        # Custom logic before updating a book
-        response = super().update(request, *args, **kwargs)
-        # Custom logic after updating a book
-        return response
+#     def update(self, request, *args, **kwargs):
+#         # Custom logic before updating a book
+#         response = super().update(request, *args, **kwargs)
+#         # Custom logic after updating a book
+#         return response
     
-    # Returns the permission classes based on the request method.
-    def get_permissions(self):
-        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
-            self.permission_classes = [IsAuthenticated]
-        else:
-            self.permission_classes = [AllowAny]
-        return super().get_permissions()
+#     # Returns the permission classes based on the request method.
+#     def get_permissions(self):
+#         if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+#             self.permission_classes = [IsAuthenticated]
+#         else:
+#             self.permission_classes = [AllowAny]
+#         return super().get_permissions()
 
