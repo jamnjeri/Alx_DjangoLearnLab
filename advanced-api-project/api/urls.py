fab20fec8528ffb44cc3241fb3a8623c74666f4b
Jamila <jamnjeri@gmail.com>
2024-09-07 12:54:04 +0300

    Task 1 first commit

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+# from .views import BookListView, BookDetailView
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    # path('books/', BookListView.as_view(), name='book-list'),
+    # path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('', include(router.urls)),
+]
