18719485c2321143ee56200eaa631c87cfb30438
Jamila <jamnjeri@gmail.com>
2024-09-08 21:00:39 +0300

    Re-adding back to ensure both tests pass

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
+
+# View for listing books using generics.ListAPIView
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 # Create your views here.
 class BookViewSet(viewsets.ModelViewSet):
